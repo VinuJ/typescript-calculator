142eb8fb32494b25afae044c9b31adb71bcd710d
Vinu Jey <severematrixoverload@gmail.com>
2021-11-11 02:13:48 +0000

    Implemented layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import './App.scss';
+import React, { FC } from "react";
+import "./App.scss";
 
 /*
   <props that it takes>  - directly or INTERFACE
@@ -26,12 +26,75 @@ import './App.scss';
   
 */
 
-const App:FC = () => {
+const App: FC = () => {
+  
+
+
+  const display = () => {};
+
   return (
     <div className="App">
-      Hello World!
+      <div className="container">
+        <div className="grid">
+          <div onClick={display} className="dis"></div>
+          <div onClick={display} className="calcButton AC tomato">
+            AC
+          </div>
+          <div onClick={display} className="calcButton C tomato">
+            C
+          </div>
+          <div onClick={display} className="calcButton divide">
+            /
+          </div>
+          <div onClick={display} className="calcButton multiply">
+            X
+          </div>
+          <div onClick={display} className="calcButton seven dark-grey">
+            7
+          </div>
+          <div onClick={display} className="calcButton eight dark-grey">
+            8
+          </div>
+          <div onClick={display} className="calcButton nine dark-grey">
+            9
+          </div>
+          <div onClick={display} className="calcButton minus">
+            -
+          </div>
+          <div onClick={display} className="calcButton four dark-grey">
+            4
+          </div>
+          <div onClick={display} className="calcButton five dark-grey">
+            5
+          </div>
+          <div onClick={display} className="calcButton six dark-grey">
+            6
+          </div>
+          <div onClick={display} className="calcButton plus">
+            +
+          </div>
+          <div onClick={display} className="calcButton one dark-grey">
+            1
+          </div>
+          <div onClick={display} className="calcButton two dark-grey">
+            2
+          </div>
+          <div onClick={display} className="calcButton three dark-grey">
+            3
+          </div>
+          <div onClick={display} className="calcButton equal blue">
+            =
+          </div>
+          <div onClick={display} className="calcButton zero dark-grey">
+            0
+          </div>
+          <div onClick={display} className="calcButton dot dark-grey">
+            .
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
